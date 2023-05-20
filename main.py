201611b0a59423f63ea4404db2a571913eb8dfc7
Raziye Nikookolah <r.nikookolah@gmail.com>
2023-05-20 12:19:39 +0000

    add router to main file

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from config import engin
 import model
+import router
 
 app = FastAPI()
+
+app.include_router(router.router, prefix="/book", tags=["book"])
 
 
 @app.get("/")
